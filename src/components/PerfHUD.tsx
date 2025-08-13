33643944a0953450ca38b3b6f97e6f0b354f8e3a
Michael Bianco <beyawnko290@gmail.com>
2025-08-12 19:39:27 -0500

    refactor: store only memory info in PerfHUD

--- src/components/PerfHUD.tsx
+++ src/components/PerfHUD.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { useFrame, useThree } from '@react-three/fiber'
 
 /** Simple dev-only HUD showing renderer statistics */
@@ -6,12 +6,12 @@ const UPDATE_INTERVAL = 0.5 // seconds
 
 export function PerfHUD() {
   const { gl } = useThree()
-  const [info, setInfo] = useState(gl.info)
-  const lastUpdate = React.useRef(0)
+  const [memoryInfo, setMemoryInfo] = useState(gl.info.memory)
+  const lastUpdate = useRef(0)
   useFrame((state) => {
     if (state.clock.elapsedTime - lastUpdate.current > UPDATE_INTERVAL) {
       lastUpdate.current = state.clock.elapsedTime
-      setInfo({ ...gl.info })
+      setMemoryInfo({ ...gl.info.memory })
     }
   })
 
@@ -27,8 +27,8 @@ export function PerfHUD() {
         fontSize: 10,
       }}
     >
-      <div>Geometries: {info.memory.geometries}</div>
-      <div>Textures: {info.memory.textures}</div>
+      <div>Geometries: {memoryInfo.geometries}</div>
+      <div>Textures: {memoryInfo.textures}</div>
     </div>
   )
 }
